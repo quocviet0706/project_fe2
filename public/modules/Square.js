22d809fbcf81af7e714be0d8505b7081b1b942d2
pn09021997 <pn0921997@gmail.com>
2021-09-25 04:24:48 +0700

    Update repo to branch phuong

---
+++ public/modules/Square.js
@@ -0,0 +1,11 @@
+//module
+export default class Square{
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+  
+  getArea() {
+    return this.width * this.height;
+  }
+}
